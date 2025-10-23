f4ee2c167562d4160b47bb4e8f4e3b8638474218
Ricardo cañas <ricardo.canas1@udea.edu.co>
2025-10-22 23:56:17 -0500

    Agregar las clases Album y  Artista

---
+++ Album.h
@@ -0,0 +1,15 @@
+#ifndef ALBUM_H
+#define ALBUM_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Almbun
+{
+    string listaAlbum;
+
+public:
+    Almbun() {}
+};
+
+#endif // ALBUM_H
